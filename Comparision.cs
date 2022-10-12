89be62c78df821dbcc8677223c3e956dfb7601d5
himanshu01031997 <hs010397@gmail.com>
2022-10-12 23:56:30 +0530

    comparing line

--- Comparision.cs
+++ Comparision.cs
@@ -7,21 +7,24 @@ using System.Threading.Tasks;
 namespace LineComparision
 {
 
+
     public class Comparision
     {
         int x1, y1, x2, y2;
 
-        public void value(int x1, int y1, int x2, int y2)
+        public Comparision(int x1, int y1, int x2, int y2)//constructor
         {
             this.x1 = x1;
             this.y1 = y1;
             this.x2 = x2;
             this.y2 = y2;
         }
-        public void calculate()
+        public double calculate()
         {
             double length = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
-            Console.WriteLine("Length of the line is {0}", length);
+            //   Console.WriteLine("length of the line is {0}",length);
+            return length;
+
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -5,9 +5,22 @@
         static void Main(string[] args)
         {
             //Console.WriteLine("welcome to line comparision");
-            Comparision comparision = new Comparision();
-            comparision.value(1,2,4,6);
-            comparision.calculate();
+            //Comparision comparision = new Comparision();
+            //comparision.value(1,2,4,6);
+            //comparision.calculate();
+            Comparision compare1 = new Comparision(2, 9, 8, 7);//parameterised constructor
+            double line1 = compare1.calculate();
+            Comparision compare2 = new Comparision(2, 9, 8, 7);
+            double line2 = compare2.calculate();
+
+            if (line1.CompareTo(line2) == 0)
+            {
+                Console.WriteLine("line1 and line2 are equal");
+            }
+            else
+            {
+                Console.WriteLine("line1 and line2 are not equal");
+            }
         }
     }
 }
