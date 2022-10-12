93915f44fe7e99904b07b3a9beda39625e80ed6e
himanshu01031997 <hs010397@gmail.com>
2022-10-12 23:41:31 +0530

    calculate length of line

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace LineComparision
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("welcome to line comparision");
+            Comparision comparision = new Comparision();
+            comparision.value(3, 5, 8, 7);
+            comparision.calculate();
+        }
+    }
+}
