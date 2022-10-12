4ccee900d5467e67f82f75da224835fbb15afb0a
himanshu01031997 <hs010397@gmail.com>
2022-10-12 23:46:59 +0530

    welcome message

--- Program.cs
+++ Program.cs
@@ -4,10 +4,8 @@
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine("welcome to line comparision");
-            Comparision comparision = new Comparision();
-            comparision.value(3, 5, 8, 7);
-            comparision.calculate();
+            Console.WriteLine("welcome to line comparision");
+          
         }
     }
 }
